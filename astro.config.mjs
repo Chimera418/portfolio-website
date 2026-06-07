7144ce15fc1a3338a1d4180f6a23c8a34da3752e
Aneesh Sagar Reddy <aneeshreddy754@gmail.com>
2026-06-07 15:48:32 +0530

    chore: explicitly set base path and server port

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,10 @@ import vercel from '@astrojs/vercel';
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  server: {
+    port: 3000,
+  },
+  base: process.env.NODE_ENV === 'production' ? '/' : '/',
   vite: {
     plugins: [tailwindcss()]
   },
